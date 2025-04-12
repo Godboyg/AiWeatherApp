2ac7e0f07315bcc6585c207bfafd9727fc6d612c
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-04-12 15:53:30 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,11 @@ export default function RootLayout({ children }) {
         <title>
           Weather APP
         </title>
+        <script 
+         async 
+         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2915175777820694"
+         crossorigin="anonymous">
+        </script>
       </head>
       <body
         className=""
