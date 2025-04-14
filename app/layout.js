4c148cab1ecddbf8e556a4216cb797d03572a4fa
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-04-14 07:45:29 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta name="monetag" content="c84e1c3e9f3ab77c08b451ef6a75328c">
+        <meta name="monetag" content="c84e1c3e9f3ab77c08b451ef6a75328c" />
         <title>
           Weather APP
         </title>
