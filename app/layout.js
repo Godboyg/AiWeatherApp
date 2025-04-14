dd8fad48f56ec436f7319ad1111939dd9b5c57a6
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-04-14 07:36:57 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,6 @@ export default function RootLayout({ children }) {
          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2915175777820694"
          crossorigin="anonymous">
         </script>
-           <meta name="monetag" content="c84e1c3e9f3ab77c08b451ef6a75328c">
       </head>
       <body
         className=""
