c388864436644cdded33626ed36a768cf307cd88
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-04-14 07:51:41 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="monetag" content="c84e1c3e9f3ab77c08b451ef6a75328c" />
+        <script 
+         src="https://kulroakonsu.net/88/tag.min.js" 
+         data-zone="142172" async data-cfasync="false">
+        </script>
         <title>
           Weather APP
         </title>
